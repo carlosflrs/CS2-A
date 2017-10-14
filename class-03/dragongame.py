d124570cccc673cc839b4c88341053481a8e118a
carlosflrs <carlos.flrsarlln@gmail.com>
2017-10-13 21:23:06 -0700

    ..

--- class-03/dragongame.py
+++ class-03/dragongame.py
@@ -5,7 +5,7 @@ import random
 # after 'def' is the name of the function
 # to call this function we type the name followed by parenthesis ().
 def display_intro():
-    print('''You are in a land full of dragons. In front of you, you see two caves.
+    print('''You are in a land full of dragons... In front of you, you see two caves.
 In one cave, the dragon is friendly and will share his treasure with you.
 The other dragon is greedy and hungry, and will eat you on sight.''') # Multi-line string
     print()
